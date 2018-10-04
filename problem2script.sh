75c2b07904c882cc91673fdc609b3532a8ab5541
Emily Mears <emears@nd.edu>
2018-10-04 19:48:28 -0400

    this is my script attempt for problem2 completed

--- problem2script.sh
+++ problem2script.sh
@@ -3,6 +3,8 @@
 # 2) gender, yearsExperience, and wage for the lowest earner
 # 3) the number of females in the top ten earners for this dataset 
 
-#usage: bash problem2script.sh filename.file
+#usage: bash problem2script.sh filename.file line_number
 
-cat $file | sort -n 
+cat $file | sort -n -t, -k4 | echo "this is the information of the person with highest paying earner" | head 1 >> problemtwoanswers.txt 
+cat $file | echo "this is the information of the person with the lowest paying earner" | head 3295  >> problemtwoanswers.txt
+cat $file | echo "this is the number of females in the top ten earners for this dataset" | head 10 |grep -w "female" >> problemtwoanswers.txt 
